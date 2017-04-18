cfde7abc98936fe7a2c4c66c6e7c975616c24b65
Key <https://github.com/Ivashthyshyn/YouNews>
2017-04-18 21:08:23 +0300

    use GreenDao (add a question to the database)

--- app/src/main/java/com/example/key/quiz/TrialActivity.java
+++ app/src/main/java/com/example/key/quiz/TrialActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 
+
+
 public class TrialActivity extends AppCompatActivity {
 
     private QuestionDao mQuestionDao;
+    private RepositoryDao mRepositoryDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +18,20 @@ public class TrialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trial);
         DaoSession daoSession = ((QuizApplication)getApplication()).getDaoSession();
         mQuestionDao = daoSession.getQuestionDao();
+        mRepositoryDao = daoSession.getRepositoryDao();
+
         //fill database Question
-        for (int i = 0; i < 10; i++) {
+
             Question question = new Question();
             question.setContent("Як створити найкращий додаток у цілому світі");
+            question.setRemoteId(1L);
             mQuestionDao.insert(question);
-
-        }
+            Repository repository = new Repository();
+            repository.setAnswer("Я не знаю");
+        repository.setUserRemoteId(1L);
+            mRepositoryDao.insert(repository);
         TextView questionText = (TextView)findViewById(R.id.question);
-
+        Long questionId = question.getId();
+        questionText.setText(question.getContent());
     }
 }
